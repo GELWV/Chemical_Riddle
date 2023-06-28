81bdb0c5c18c77e771ca5a4f4126b64d8bd4349d
Leonardo9119_aH4 <leonardoartursasse@gmail.com>
2023-06-28 15:45:45 -0300

    logica de pontuacao e das questoes

--- assets/main.js
+++ assets/main.js
@@ -6,13 +6,15 @@ let vida = 0
 let pontuacao = 0
 let respostaCerta = null
 let respostaRecebida = null
-let perguntasJaForam = [1]
+let perguntasJaForam = [null]
 let acerto = false //booleana de controle de acerto
 let Erros = 1 //variavel que eh usada para calcular a pontuacao
+let EmJogo = false
 
 function perguntar(){
     pergunta.innerText = jsonQuizData[seletor].questao
     respostaCerta = jsonQuizData[seletor].resposta
+    //respostaRecebida = 
     if(respostaCerta==respostaRecebida){
         acertou()
     }
@@ -27,13 +29,11 @@ function acertou(){
     pontuacao =+ 1/Erros
     perguntasJaForam.push(seletor)
     seletor = aleatorio()
-    return
 }
 function errou(){
     acerto = false
     Erros =+ 1
     vida--
-    return
 }
 function aleatorio(){
     let X = Math.floor(Math.random()*118)+1
@@ -42,3 +42,38 @@ function aleatorio(){
     }
     return X
 }
+function reset(){
+    acerto = false
+    perguntasJaForam = [null]
+    respostaCerta = null    
+    respostaRecebida = null
+    vida = 10
+    Erros = 1
+    pontuacao = 0
+    seletor = aleatorio()
+    EmJogo = true
+}
+function Default(){
+    pergunta = document.querySelector("div.pergunta")
+    seletor = -1 
+    vida = 0
+    pontuacao = 0
+    respostaCerta = null
+    respostaRecebida = null
+    perguntasJaForam = [null]
+    acerto = false 
+    Erros = 1
+    EmJogo = false
+}
+
+//popup de start
+while(perguntasJaForam.length == 118 && vida > 0){
+    perguntar()
+}
+if(perguntasJaForam.length == 118){
+    //popup de vitoria
+}
+if(vida<=0){
+    //popup de derrota
+}
+Default()

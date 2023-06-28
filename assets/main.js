71d9c96a4c95f036caa93794f3a47829c5631246
Dudisdu99 <duduflesch2007@gmail.com>
2023-06-28 16:03:26 -0300

    reorgnizei e add mais css da tabela

--- assets/main.js
+++ assets/main.js
@@ -2,3 +2,4 @@ const pergunta = document.querySelector("div.pergunta")
 const requisicao = await fetch("assets/quiz.json")
 const jsonQuizData = await requisicao.json()
 pergunta.innerText = jsonQuizData[0].questao
+

bee708336596566c962066f744e3a41c2f615662
Leonardo9119_aH4 <leonardoartursasse@gmail.com>
2023-06-28 13:43:15 -0300

    scripts

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,25 @@
 let pergunta = document.querySelector("div.pergunta")
-const requisicao = await fetch("assets/quiz.json")
-const jsonQuizData = await requisicao.json()
-pergunta.innerText = jsonQuizData[0].questao
+let seletor = -1
+let vida = 0
+let pontuacao = 0
+let respostaCerta = null
+let respostaRecebida = null
+
+
+function perguntar(){
+    let requisicao = await fetch("assets/quiz.json")
+    let jsonQuizData = await requisicao.json()
+    pergunta.innerText = jsonQuizData[seletor].questao
+    respostaCerta = jsonQuizData[seletor].resposta
+    if(respostaCerta==respostaRecebida){
+        acertou()
+    }
+    else{
+        errou()
+    }
+    seletor++
+    return
+}
+function acerto(){
+
+}

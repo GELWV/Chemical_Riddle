38d39b601f2b0801a76cff6fd6ac13302fd20d8b
Leonardo9119_aH4 <leonardoartursasse@gmail.com>
2023-06-28 15:14:09 -0300

    script desenvolvimento

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,16 @@
+const requisicao = await fetch("assets/quiz.json")
+const jsonQuizData = await requisicao.json()
 let pergunta = document.querySelector("div.pergunta")
-let seletor = -1
+let seletor = -1 //seleciona a pergunta no JSON
 let vida = 0
 let pontuacao = 0
 let respostaCerta = null
 let respostaRecebida = null
-
+let perguntasJaForam = [1]
+let acerto = false //booleana de controle de acerto
+let Erros = 1 //variavel que eh usada para calcular a pontuacao
 
 function perguntar(){
-    let requisicao = await fetch("assets/quiz.json")
-    let jsonQuizData = await requisicao.json()
     pergunta.innerText = jsonQuizData[seletor].questao
     respostaCerta = jsonQuizData[seletor].resposta
     if(respostaCerta==respostaRecebida){
@@ -17,9 +19,26 @@ function perguntar(){
     else{
         errou()
     }
-    seletor++
     return
 }
-function acerto(){
-
+function acertou(){
+    acerto = true
+    Erros = 1
+    pontuacao =+ 1/Erros
+    perguntasJaForam.push(seletor)
+    seletor = aleatorio()
+    return
+}
+function errou(){
+    acerto = false
+    Erros =+ 1
+    vida--
+    return
+}
+function aleatorio(){
+    let X = Math.floor(Math.random()*118)+1
+    if(perguntasJaForam.includes(X)) {
+        aleatorio()
+    }
+    return X
 }
